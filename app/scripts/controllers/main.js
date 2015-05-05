ea8198080517cd1cf669e652735e32d0d431d76c
Leszek <leszek.kasperczyk@gmail.com>
2015-05-05 10:22:14 +0100

    reversed dates on main view and css changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,44 +7,44 @@
  * Controller of the pearsonAngApp
  */
 angular.module('pearsonAngApp')
-  .controller('MainCtrl', function($scope, $http, statusColour, $location, selectDay, substractDate) {
+    .controller('MainCtrl', function ($scope, $http, statusColour, $location, selectDay, substractDate) {
 
-    $scope.lastupdated = [];
-    $scope.dane = [];
-    $scope.daty = [];
-    
-    $scope.setDay = function(day){
-        selectDay.addSelectedDay(day);
-    };
+        $scope.lastupdated = [];
+        $scope.dane = [];
+        $scope.daty = [];
 
-    $http.get('https://pearsonmarketingcloud-test.apigee.net/psp/v1/productstatus.do')
-      .success(function(data) {
-        console.log('DATA pass');
-
-        $scope.lastupdated = {
-          hour: substractDate.subs.hour(data.lastupdated),
-          date: substractDate.subs.date(data.lastupdated)
+        $scope.setDay = function (day) {
+            selectDay.addSelectedDay(day);
         };
 
-        $scope.dane = data.products; // response data
-        $scope.daty = data.products[0].updates; // response data     
-        
-        for (var i = 0; i < $scope.dane.length; i++) {
-          for (var k = 0; k < $scope.dane[i].updates.length; k++) {
-            $scope.dane[i].updates[k].date = {
-              hour: substractDate.subs.hour($scope.dane[i].updates[k].date),
-              date: substractDate.subs.date($scope.dane[i].updates[k].date)
-            };
-          }
-        }
+        $http.get('https://pearsonmarketingcloud-test.apigee.net/psp/v1/productstatus.do')
+            .success(function (data) {
+                console.log('DATA pass');
 
-      }).error(function() {
-        console.log('DATA failed');
-      });
+                $scope.lastupdated = {
+                    hour: substractDate.subs.hour(data.lastupdated),
+                    date: substractDate.subs.date(data.lastupdated)
+                };
 
-    $scope.getStatus = function(a) {
-      return statusColour.list(a);
-    };
+                $scope.dane = data.products; // response data
+                $scope.daty = data.products[0].updates; // response data     
+
+                for (var i = 0; i < $scope.dane.length; i++) {
+                    for (var k = 0; k < $scope.dane[i].updates.length; k++) {
+                        $scope.dane[i].updates[k].date = {
+                            hour: substractDate.subs.hour($scope.dane[i].updates[k].date),
+                            date: substractDate.subs.date($scope.dane[i].updates[k].date)
+                        };
+                    }
+                }
+
+            }).error(function () {
+                console.log('DATA failed');
+            });
+
+        $scope.getStatus = function (a) {
+            return statusColour.list(a);
+        };
 
 
-  });
+    });
