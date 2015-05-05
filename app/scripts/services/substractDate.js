5754f036e25b777eeeed03bae787b124e295f8d4
Leszek <leszek.kasperczyk@gmail.com>
2015-05-05 17:54:46 +0100

    date issue fixed

--- app/scripts/services/substractDate.js
+++ app/scripts/services/substractDate.js
@@ -14,21 +14,21 @@ service('substractDate',
     function () {
 
         var substract = function (elem, first, second) {
-                return String(new Date(elem)).slice(first, second);
+                return String((new Date(elem)).toUTCString()).slice(first, second);
             },
 
             subs = {
                 hour: function (elem) {
-                    return substract(elem, 16, 21);
+                    return substract(elem, 16, 22);
                 },
                 date: function (elem) {
-                    return substract(elem, 4, 15);
+                    return substract(elem, 4, 16);
                 },
                 dayName: function (elem) {
                     return substract(elem, 0, 3);
                 },
                 dateNoYear: function (elem) {
-                    return substract(elem, 4, 10);
+                    return substract(elem, 4, 11);
                 },
                 fullDateObj: function (jsonPair) {
                     for (var k = 0; k < jsonPair.length; k++) {
